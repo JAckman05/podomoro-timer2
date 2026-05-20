43f2fe7697a4bdba543b3d441850053afbf4a5bf
JAckman05 <113114139@gms.tcu.edu.tw>
2026-05-20 16:03:57 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/podomoro-timer2/'
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
